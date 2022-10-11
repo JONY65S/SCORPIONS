186748c15c645fbf8e8ccc88dc5b1b76e4753a86
JONY65S <100656956+JONY65S@users.noreply.github.com>
2022-10-10 19:48:27 -0500

    Add files via upload

---
+++ WX01/perfilponente.php
@@ -0,0 +1,488 @@
+<html lang="es">
+    <head>
+        
+       <link href="icono.ico" type="image/x-icon" rel="shortcut icon" />
+        
+        <title>Congreso de Matemáticas</title>
+       <meta charset="UTF-8"> 
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+         
+  
+        
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/estilosponente.css">
+        <link rel="stylesheet" type="text/css" href="css/styleNav2.css">
+        <link rel="stylesheet" href="icon.css">
+         
+         
+        <style>
+            
+          .botonperfil{
+                background: transparent;
+                background-image: url("img/perfil1.png");
+                border-color: transparent;
+                background-size: cover;
+                background-position: center;
+                display: inline-block;
+                border: none;
+                padding: 20px;
+                width: 180px;
+                border-radius: 0px;
+                height: 180px;
+                transition: all 0.5s;
+                cursor: pointer;
+                position: absolute;
+                top: 42%;
+                left: 15%;
+                transform: translate(-30%, -40%);
+                
+                }
+                .botonperfil:hover
+                {
+                width: 230px;
+                height: 230px;
+                filter: invert(59%) sepia(73%) saturate(310%) hue-rotate(10deg) brightness(92%) contrast(97%);
+                }
+
+                @media (max-width: 768px){
+          
+          .botonperfil {
+            position: absolute;
+            margin-left: 0;
+            top: 20%;
+            width: 150px;
+            height: 150px;
+          }
+          .botonperfil:hover
+                {
+                width: 190px;
+                height: 190px;
+                filter: invert(59%) sepia(73%) saturate(310%) hue-rotate(10deg) brightness(92%) contrast(97%);
+                }
+        }
+          
+                .botonhistorial{
+                background: transparent;
+                background-image: url("img/historial1.png");
+                border-color: transparent;
+                background-size: cover;
+                background-position: center;
+                display: inline-block;
+                border: none;
+                padding: 20px;
+                width: 180px;
+                border-radius: 0px;
+                height: 180px;
+                transition: all 0.5s;
+                cursor: pointer;
+                position: absolute;
+                top: 42%;
+                left: 35%;
+                transform: translate(-30%, -40%);
+               
+                
+                }
+                .botonhistorial:hover
+                {
+                width: 230px;
+                height: 230px;
+                filter: invert(59%) sepia(73%) saturate(310%) hue-rotate(10deg) brightness(92%) contrast(97%);
+                }
+                @media (max-width: 768px){
+                .botonhistorial {
+            position: absolute;
+            margin-left: 0;
+            top: 20%;
+            left: 63%;
+            width: 150px;
+            height: 150px;
+          }
+          .botonhistorial:hover
+                {
+                width: 190px;
+                height: 190px;
+                filter: invert(59%) sepia(73%) saturate(310%) hue-rotate(10deg) brightness(92%) contrast(97%);
+                }
+        }
+                .botonpagos{
+                background: transparent;
+                background-image: url("img/pagos1.png");
+                border-color: transparent;
+                background-size: cover;
+                background-position: center;
+                display: inline-block;
+                border: none;
+                padding: 20px;
+                width: 180px;
+                border-radius: 0px;
+                height: 180px;
+                transition: all 0.5s;
+                cursor: pointer;
+                position: absolute;
+                top: 42%;
+                left: 55%;
+                transform: translate(-30%, -40%);
+                
+                }
+                .botonpagos:hover
+                {
+                width: 230px;
+                height: 230px;
+               
+                filter: invert(59%) sepia(73%) saturate(310%) hue-rotate(10deg) brightness(92%) contrast(97%);
+                }
+                @media (max-width: 768px){
+          
+          .botonpagos {
+            position: absolute;
+            margin-left: 0;
+            top: 65%;
+            left: 15%; 
+            width: 150px;
+            height: 150px;
+          }
+          .botonpagos:hover
+                {
+                width: 190px;
+                height: 190px;
+                filter: invert(59%) sepia(73%) saturate(310%) hue-rotate(10deg) brightness(92%) contrast(97%);
+                }
+        }
+                .botonregistro{
+                
+                background: transparent;
+                background-image: url("img/registro1.png");
+                border-color: transparent;
+                background-size: cover;
+                background-position: center;
+                display: inline-block;
+                border: none;
+                padding: 20px;
+                width: 180px;
+                border-radius: 0px;
+                height: 180px;
+                transition: all 0.5s;
+                cursor: pointer;
+                position: absolute;
+                top: 42%;
+                left: 75%;
+                transform: translate(-30%, -40%);
+                  
+                }
+                .botonregistro:hover
+                {
+                width: 230px;
+                height: 230px;
+             
+                filter: invert(59%) sepia(73%) saturate(310%) hue-rotate(10deg) brightness(92%) contrast(97%);
+                }
+                @media (max-width: 768px){
+          
+          .botonregistro {
+            position: absolute;
+            margin-left: 0;
+            top: 65%;
+            left: 62%; 
+            width: 150px;
+            height: 150px;
+          }
+          .botonregistro:hover
+                {
+                width: 190px;
+                height: 190px;
+                filter: invert(59%) sepia(73%) saturate(310%) hue-rotate(10deg) brightness(92%) contrast(97%);
+                }
+        }
+        
+        .botonponencias{
+                background: transparent;
+                background-image: url("img/ponencia1.png");
+                border-color: transparent;
+                background-size: cover;
+                background-position: center;
+                display: inline-block;
+                border: none;
+                padding: 20px;
+                width: 180px;
+                border-radius: 0px;
+                height: 180px;
+                transition: all 0.5s;
+                cursor: pointer;
+                position: absolute;
+                top: 42%;
+                left: 15%;
+                transform: translate(-30%, -40%);
+               display: none;
+                }
+                .botonponencias:hover
+                {
+                width: 230px;
+                height: 230px;
+                filter: invert(59%) sepia(73%) saturate(310%) hue-rotate(10deg) brightness(92%) contrast(97%);
+                }
+
+                @media (max-width: 768px){
+          
+          .botonponencias {
+            position: absolute;
+            margin-left: 0;
+            top: 20%;
+            width: 150px;
+            height: 150px;
+          }
+          .botonponencias:hover
+                {
+                width: 190px;
+                height: 190px;
+                filter: invert(59%) sepia(73%) saturate(310%) hue-rotate(10deg) brightness(92%) contrast(97%);
+                }
+        }
+        
+        .botoncarteles{
+                background: transparent;
+                background-image: url("img/carteles1.png");
+                border-color: transparent;
+                background-size: cover;
+                background-position: center;
+                display: inline-block;
+                border: none;
+                padding: 20px;
+                width: 180px;
+                border-radius: 0px;
+                height: 180px;
+                transition: all 0.5s;
+                cursor: pointer;
+                position: absolute;
+                top: 42%;
+                left: 45%;
+                transform: translate(-30%, -40%);
+                display:none;
+                
+                }
+                .botoncarteles:hover
+                {
+                width: 230px;
+                height: 230px;
+                filter: invert(59%) sepia(73%) saturate(310%) hue-rotate(10deg) brightness(92%) contrast(97%);
+                }
+                @media (max-width: 768px){
+                .botoncarteles {
+            position: absolute;
+            margin-left: 0;
+            top: 20%;
+            left: 63%;
+            width: 150px;
+            height: 150px;
+          }
+          .botoncarteles:hover
+                {
+                width: 190px;
+                height: 190px;
+                filter: invert(59%) sepia(73%) saturate(310%) hue-rotate(10deg) brightness(92%) contrast(97%);
+                }
+        }
+        
+        .botontalleres{
+                background: transparent;
+                background-image: url("img/talleres1.png");
+                border-color: transparent;
+                background-size: cover;
+                background-position: center;
+                display: inline-block;
+                border: none;
+                padding: 20px;
+                width: 180px;
+                border-radius: 0px;
+                height: 180px;
+                transition: all 0.5s;
+                cursor: pointer;
+                position: absolute;
+                top: 42%;
+                left: 75%;
+                transform: translate(-30%, -40%);
+                display:none;
+                }
+                .botontalleres:hover
+                {
+                width: 230px;
+                height: 230px;
+               
+                filter: invert(59%) sepia(73%) saturate(310%) hue-rotate(10deg) brightness(92%) contrast(97%);
+                }
+                @media (max-width: 768px){
+          
+          .botontalleres {
+            position: absolute;
+            margin-left: 0;
+            top: 65%;
+            left: 15%; 
+            width: 150px;
+            height: 150px;
+          }
+          .botontalleres:hover
+                {
+                width: 190px;
+                height: 190px;
+                filter: invert(59%) sepia(73%) saturate(310%) hue-rotate(10deg) brightness(92%) contrast(97%);
+                }
+        }
+        
+       
+        </style>
+        
+       
+    </head>
+    <body>
+        <!--
+Encabezado de la página */
+        */banner, menu, carrusel, cuadro iniciar, cuadro fechas, -->
+        <!-- Baner -->
+        
+         
+         <div id="wrapper" > <center> <img src="img/banpru.jpg" class="baner"/> </center>  </div>
+        
+        
+       <!-- Barra de menus -->
+         <div>
+            <header>  
+                <input type="checkbox" id="btn-menu"> 
+                <label for="btn-menu"><img src="img/menuicono.png" alt=""></label>
+                
+                
+                
+               
+                <!-- Barra de menu Principal --> 
+
+               <div class="menu" style="z-index: 2; top: 42%">
+                    <ul>
+                        <li> <a href="">Inicio</a></li>
+                        <li> <a href="">Memorias</a></li>
+                        <li> <a href="">Convocatoria</a></li> 
+                        <li> <a href="">Inscripción</a></li>
+                        <li> <a href="">Comité Organizador</a></li>
+                        <li> <a href=""><img class="alineadoicono" src="img/iniciaricono.png">&nbsp;Iniciar Sesión</a></li>
+                    </ul>  
+                </div> 
+
+                
+                
+           
+            <input type="checkbox" id="btn-menu2"> 
+                <label for="btn-menu2"><img src="img/icono_informacion.png" alt=""> </label>
+                
+                <div class="menu2" style="z-index: 2; top:50%">           
+                    <ul>    
+                      <li> <a href=""><img class="alineadoicono" src="img/icono_informacion.png"> </a></li>        
+                        <li> <a href="">Ponencias</a></li>
+                        <li> <a href="">Carteles</a></li> 
+                        <li> <a href="">Talleres</a></li>
+                    </ul> 
+                </div> 
+               
+     
+                
+                <!-- Barra de menu Secundario-->
+
+                <div class="menuRB" style="z-index: 1; top: 54%;"> 
+                    <ul>
+                        <li><a href="#"><span class="first" onClick="ico2()"><i class="icon-book"></i></span>Alumno</a></li>
+                        <li><a href="#"><span class="second" onClick="ico1()"><i class="icon-file-text2"></i></span>Ponente</a></li>
+                        <li><a href="#"><span class="third"><i class="icon-mic"></i></span>Evaluador</a></li>
+                        <li><a href="#"><span class="fourth"><i class="icon-stack"></i></span>Memorias</a></li>
+                        <li><a href="#"><span class="fifth"><i class="icon-key"></i></span>Admin</a></li>
+                    </ul>
+                </div>
+                
+                
+            </header>
+
+        </div>
+
+        <!-- Barra de menu Secundario - Movil-->
+
+       <div class="nav-bar" >
+            <a href="#" class="icond icon-book" target="_blank"> </a>
+            <a href="#" class="icond icon-file-text2" target="_blank"> </a>
+            <a href="#" class="icond icon-mic" target="_blank"> </a>
+            <a href="#" class="icond icon-stack" target="_blank"> </a>
+            <a href="#" class="icond icon-key" target="_blank"> </a>
+        </div> 
+
+        <!-- Iconos -->
+   
+       <!--<h1>PONENTE</h1> -->
+  
+     
+      <div class="rectangulo">
+                    <div id="User">
+                    <div class="botonperfil"> </div>
+                    <div class="botonhistorial"> </div>
+                    <div class="botonpagos"> </div>
+                    <div class="botonregistro"> </div>
+                    </div>
+                    
+                    <div class="botonponencias" id="Pone"></div>
+                    <div class="botoncarteles" id="Pone1"></div>
+                    <div class="botontalleres" id="Pone2"></div>
+                   
+                    
+       <div class="contacto_padre">
+                       <img src="img/escudo-blanco.png">
+                        <div class="contacto">
+                        <h1>CONTACTO</h1>
+                       <h2>SEDE</h2> 
+                      <p>
+                        Facultad de Estudios Superiores Cuautitlán
+                      </p> 
+                      <h2>UBICACIÓN</h2> 
+                      <p>
+                        Km 2.5 carretera Cuautitlán-Teoloyucan, San Sebastián Xhala, Cuautitlán Izcalli;,
+                        Estado de México.
+                        C.P. 54714.
+                      </p>
+                      <h2>TELÉFONO</h2>
+                     <p> (55) 56 23 18 86 / (55) 56 23 18 90 </p>  
+                     <p> Para mayores informes o dudas comunicarse al Departamento de Matemáticas Edificio A8 Campo 4. </p>  
+                        </div>
+                    </div>
+       
+        <div class="creditos_padre">     
+                        <div class="creditos">
+                      <p> Hecho en México, todos los derechos reservados 2014-2022. © </p>
+                      <p> Esta página puede ser reproducida con fines no lucrativos, siempre y cuando no se mutile, se cite la fuente completa y su dirección electrónica. De otra forma requiere permiso previo por escrito de la institución.</p> 
+                      <p>
+                        Sitio Web administrado por: Departamento de Matemáticas
+                      </p>
+                        </div>
+                      
+                </div>
+      
+      </div>
+      
+       
+       <!--OCULTAR BOTONES--><!-- comment -->
+       
+    
+
+
+       <script type="text/javascript">
+           function ico1(){
+                document.getElementById('User').style.display = 'none';
+                document.getElementById('Pone').style.display = 'block';   
+                document.getElementById('Pone1').style.display = 'block'; 
+                document.getElementById('Pone2').style.display = 'block'; 
+            }
+
+            function ico2(){
+                document.getElementById('User').style.display = 'block';
+                document.getElementById('Pone').style.display = 'none'; 
+                document.getElementById('Pone1').style.display = 'none';
+                document.getElementById('Pone2').style.display = 'none';
+            } 
+
+        </script>
+      
+                    
+         
+    </body>
+</html>
